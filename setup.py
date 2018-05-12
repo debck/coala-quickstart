8329147b4456beb93cd96a2891b3ee7b9ffd9bb4
John Vandenberg <jayvdb@gmail.com>
2018-05-12 23:12:10 +0700

    Build setup.py and requirements files using moban
    coala-mobans provides a set of metadata common to
    all repositories, with local overrides.
    
    Related to https://github.com/coala/meta/issues/117

--- setup.py
+++ setup.py
@@ -1,12 +1,60 @@
 #!/usr/bin/env python3
 
 import locale
+import platform
+import sys
+from subprocess import call
+
+import setuptools.command.build_py
 from setuptools import find_packages, setup
+from setuptools.command.test import test as TestCommand
 
 try:
-    locale.getlocale()
+    lc = locale.getlocale()
+    pf = platform.system()
+    if pf != 'Windows' and lc == (None, None):
+        locale.setlocale(locale.LC_ALL, 'C.UTF-8')
 except (ValueError, UnicodeError):
     locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
+VERSION = '0.4.0'
+
+
+class PyTestCommand(TestCommand):
+    """
+    From https://pytest.org/latest/goodpractices.html
+    """
+    user_options = [('pytest-args=', 'a', 'Arguments to pass to py.test')]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
+
+
+class BuildDocsCommand(setuptools.command.build_py.build_py):
+    apidoc_command = (
+        'sphinx-apidoc', '-f', '-o', 'docs',
+        'coala_quickstart'
+    )
+    make_command = ('make', '-C', 'docs', 'html', 'SPHINXOPTS=-W')
+
+    def run(self):
+        err_no = call(self.apidoc_command)
+        if not err_no:
+            err_no = call(self.make_command)
+        sys.exit(err_no)
+
 
 with open('requirements.txt') as requirements:
     required = requirements.read().splitlines()
@@ -14,30 +62,38 @@ with open('requirements.txt') as requirements:
 with open('test-requirements.txt') as requirements:
     test_required = requirements.read().splitlines()
 
-if __name__ == "__main__":
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+extras_require = None
+data_files = None
+
+if __name__ == '__main__':
     setup(name='coala-quickstart',
-          version='0.4.0',
+          version=VERSION,
           description='A quickstart tool for coala',
-          author="The coala developers",
-          maintainer="Satwik Kansal, Adrian Zatreanu, Alexandros Dimos, "
-                     "Adhityaa Chandrasekar",
+          author='The coala developers',
+          author_email='coala.analyzer@gmail.com',
+          maintainer='Satwik Kansal, Adrian Zatreanu, Alexandros Dimos, Adhityaa Chandrasekar',
           maintainer_email=('satwikkansal@gmail.com, '
                             'adrianzatreanu1@gmail.com, '
                             'alexandros.dimos.95@gmail.com, '
                             'c.adhityaa@gmail.com'),
           url='https://github.com/coala/coala-quickstart',
           platforms='any',
-          packages=find_packages(exclude=["build.*", "*.tests.*", "*.tests"]),
+          packages=find_packages(exclude=('build.*', 'tests', 'tests.*')),
           install_requires=required,
+          extras_require=extras_require,
           tests_require=test_required,
-          license="AGPL-3.0",
-          long_description="coala-quickstart is a tool to help you "
-                           "quickly and easily get started with "
-                           "coala.",
+          package_data={'coala_quickstart': ['VERSION']},
+          license='AGPL-3.0',
+          data_files=data_files,
+          long_description=long_description,
           entry_points={
-              "console_scripts": [
-                  "coala-quickstart = coala_quickstart.coala_quickstart:main"
-                  ]},
+              'console_scripts': [
+                  'coala-quickstart = coala_quickstart.coala_quickstart:main',
+              ],
+          },
           # from http://pypi.python.org/pypi?%3Aaction=list_classifiers
           classifiers=[
               'Development Status :: 4 - Beta',
@@ -45,6 +101,7 @@ if __name__ == "__main__":
               'Environment :: Console',
               'Environment :: MacOS X',
               'Environment :: Win32 (MS Windows)',
+              'Environment :: X11 Applications :: Gnome',
 
               'Intended Audience :: Science/Research',
               'Intended Audience :: Developers',
@@ -61,4 +118,6 @@ if __name__ == "__main__":
 
               'Topic :: Scientific/Engineering :: Information Analysis',
               'Topic :: Software Development :: Quality Assurance',
-              'Topic :: Text Processing :: Linguistic'])
+              'Topic :: Text Processing :: Linguistic'],
+          cmdclass={'docs': BuildDocsCommand,
+                    'test': PyTestCommand})
